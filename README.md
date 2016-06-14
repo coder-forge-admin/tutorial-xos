503cb821153dfd3e81a72d2fe1989d490fe08a5c
Daithi Coombes <webeire@gmail.com>
2016-06-14 22:15:33 +0100

    concept gui

---
+++ README.md
@@ -0,0 +1,28 @@
+# Tutorial: Distributed X's and O's game
+
+### lessons
+
+The lessons for this tutorial are split across branches.
+
+### installation
+```bash
+git clone https://github.com/coder-forge/tutorial-xos
+cd tutorial-xos
+npm install
+meteor
+```
+
+### Flow
+
+User selects Opponant from list (see networking below)
+Oponent accepts
+Coin toss (random num generator) selects who's X and who's O
+X's start first
+
+### Networking
+
+App will listen for broadcasts from other apps on a port.
+App will broadcast across subnet (default 255.255.0.0) once every x times, and
+always on startup.
+Broadcast will include {ipAddress, username}
+App will list ip's => username's of any broadcasts made.
--- client/main.js
+++ client/main.js
@@ -3,6 +3,29 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
+Template.board.onRendered(function boardCreated(){
+
+    // #board ul li button
+    const buttons = $('#board button');
+
+    for(let x=0; x<buttons.length; x++){
+
+        let button          = $(buttons[x]),
+            widthButton     = button.width(),
+            widthTotal      = $('#board').width(),
+            heightButton    = null,
+            heightTotal     = $('#board').height();
+
+        if( (widthButton*3) > heightTotal )
+            heightButton = heightTotal / 3.5;
+        else
+            heightButton = widthTotal / 3;
+
+        button.height(heightButton*1.2);
+    }
+});
+
+/**
 Template.hello.onCreated(function helloOnCreated() {
   // counter starts at 0
   this.counter = new ReactiveVar(0);
@@ -20,3 +43,4 @@ Template.hello.events({
     instance.counter.set(instance.counter.get() + 1);
   },
 });
+*/
