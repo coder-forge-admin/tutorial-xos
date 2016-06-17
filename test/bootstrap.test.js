a695db0d8724a957c946436b4e75d69892e90b41
Daithi Coombes <webeire@gmail.com>
2016-06-17 20:23:02 +0100

    mad biscuits and hairy lemons

---
+++ test/bootstrap.test.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const path = require('path');
+
+global.dirRoot = path.normalize(__dirname+'/../');
+global.dirApp = path.normalize(__dirname+'/../'+'app/');
+global.dirTest = path.normalize(__dirname+'/../'+'test/');
--- test/unit/socket.test.js
+++ test/unit/socket.test.js
@@ -1,10 +1,27 @@
 "use strict";
 
-const nock = require('nock');
+const assert = require('chai').assert,
+    nock = require('nock'),
+    Socket = require(global.dirApp+'lib/Socket');
 
 describe('Socket', ()=>{
 
     it('Will scan for other players', (done)=>{
-        done();
+
+        const expected = {
+            ip: '127.0.0.10',
+            username: 'FooBar Mc BizBaz',
+        }
+
+        // setup nock to catch
+        const player = nock('http://127.0.0.10')
+            .get('/player/')
+            .reply('200', expected);
+
+        Socket.findPlayers((err, actual)=>{
+            assert.deepEqual(actual, expected);
+            done();
+        });
+
     });
 });
